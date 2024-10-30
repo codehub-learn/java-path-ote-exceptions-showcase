8b9d53bf5734d7df1707e825c74b16ac460880b0
Ioannis <jondan97@gmail.com>
2024-10-30 11:03:00 +0200

    git init

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+/*        System.out.println("Hello world!");
+        ArrayList<Object> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(1);
+        arrayList.add(1);
+        arrayList.add(1);
+        arrayList.add(1);
+        System.out.println(arrayList);
+
+        HashSet<Object> hashSet = new HashSet<>();
+        hashSet.add(1);
+        hashSet.add(1);
+        hashSet.add(1);
+        hashSet.add(1);
+        System.out.println(hashSet);
+
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("Java", "Programming Language");
+        hashMap.put("Java", "Backend Technology");
+        hashMap.put("C++", "Programming Language");
+        hashMap.put("Git", "Version Control System");
+        hashMap.put("JavaScript", "Programming Language");
+
+        String value = hashMap.get("Java");
+        System.out.println(value);
+
+        System.out.println(hashMap);*/
+
+        /*String[] stringsArray = new String[10];
+        System.out.println(stringsArray[1500]);*/
+        method1();
+    }
+
+
+    private static void method1(){
+        int result = 5 / 0;
+    }
+}
